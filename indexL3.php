0417ff1b67effa8687852ec93d9f32ba085dfcc7
drearam3 <andramirez@csumb.edu>
2016-02-24 22:34:49 +0000

    Associative Array for Individual Cards

--- indexL3.php
+++ indexL3.php
@@ -7,63 +7,63 @@
     </head>
     <body>
         <?php
-        //player
-        //cards
-        //result
-        //random number of cards
-        //random cards
+        player
+        cards
+        result
+        random number of cards
+        random cards
         
-        // $cardC1 = ["score" => "1", 
-        //         "suit" => "Clubs",
-        //         "rank" => "A"];
-        // $cardC2 = ["score" => "2", 
-        //         "suit" => "Clubs",
-        //         "rank" => "2"];
-        // $cardC3 = ["score" => "3", 
-        //         "suit" => "Clubs",
-        //         "rank" => "3"];
-        // $cardC4 = ["score" => "4", 
-        //         "suit" => "Clubs",
-        //         "rank" => "4"];
-        // $cardC5 = ["score" => "5", 
-        //         "suit" => "Clubs",
-        //         "rank" => "5"];
-        // $cardC6 = ["score" => "6", 
-        //         "suit" => "Clubs",
-        //         "rank" => "6"];
-        // $cardC7 = ["score" => "7", 
-        //         "suit" => "Clubs",
-        //         "rank" => "A"];
-        // $cardC8 = ["score" => "8", 
-        //         "suit" => "Clubs",
-        //         "rank" => "2"];
-        // $cardC9 = ["score" => "9", 
-        //         "suit" => "Clubs",
-        //         "rank" => "3"];
-        // $cardC10 = ["score" => "10", 
-        //         "suit" => "Clubs",
-        //         "rank" => "4"];
-        // $cardC11 = ["score" => "11", 
-        //         "suit" => "Clubs",
-        //         "rank" => "5"];
-        // $cardC12 = ["score" => "12", 
-        //         "suit" => "Clubs",
-        //         "rank" => "6"];
+        $cardC1 = ["score" => "1", 
+                "suit" => $clubSuit,
+                "rank" => "A"];
+        $cardC2 = ["score" => "2", 
+                "suit" => $clubSuit,
+                "rank" => "2"];
+        $cardC3 = ["score" => "3", 
+                "suit" => $clubSuit,
+                "rank" => "3"];
+        $cardC4 = ["score" => "4", 
+                "suit" => $clubSuit,
+                "rank" => "4"];
+        $cardC5 = ["score" => "5", 
+                "suit" => $clubSuit,
+                "rank" => "5"];
+        $cardC6 = ["score" => "6", 
+                "suit" => $clubSuit,
+                "rank" => "6"];
+        $cardC7 = ["score" => "7", 
+                "suit" => $clubSuit,
+                "rank" => "A"];
+        $cardC8 = ["score" => "8", 
+                "suit" => $clubSuit,
+                "rank" => "2"];
+        $cardC9 = ["score" => "9", 
+                "suit" => $clubSuit,
+                "rank" => "3"];
+        $cardC10 = ["score" => "10", 
+                "suit" => $clubSuit,
+                "rank" => "4"];
+        $cardC11 = ["score" => "11", 
+                "suit" => $clubSuit,
+                "rank" => "5"];
+        $cardC12 = ["score" => "12", 
+                "suit" => $clubSuit,
+                "rank" => "6"];
                 
                 
         
-        // $clubSuit = ["directory" => "img/cards/Clubs",
-        //             "name" => "Clubs"];
-        // $diamondsSuit = ["directory" => "img/cards/Diamonds",
-        //             "name" => "Diamonds"];
-        // $heartsSuit = ["directory" => "img/cards/Hearts",
-        //             "name" => "Hearts"];
-        // $spadesSuit = ["directory" => "img/cards/Spades",
-        //             "name" => "Spades"];
+        $clubSuit = ["directory" => "img/cards/Clubs",
+                    "name" => "Clubs"];
+        $diamondsSuit = ["directory" => "img/cards/Diamonds",
+                    "name" => "Diamonds"];
+        $heartsSuit = ["directory" => "img/cards/Hearts",
+                    "name" => "Hearts"];
+        $spadesSuit = ["directory" => "img/cards/Spades",
+                    "name" => "Spades"];
                     
          
-        // //indexed array of the card objects      
-        // $deck = [];
+        //indexed array of the card objects      
+        $deck = [];
 
         $player1 = ["imageName" => "",
                     "name" => $_POST["p1"]];
@@ -76,17 +76,17 @@
 
         $table = [$player1, $player2, $player3, $player4];
 
-                // ["position1" => $player1,
-                // "posiiton2" => $player2,
-                // "position3" => $player3,
-                // "position4" => $player4];
+                ["position1" => $player1,
+                "posiiton2" => $player2,
+                "position3" => $player3,
+                "position4" => $player4];
         
-        // $hand = ["player" => null,
-        //         "cards" => [], 
-        //         "scores" => ""];
+        $hand = ["player" => null,
+                "cards" => [], 
+                "scores" => ""];
 
-        // $game= ["location" => $table,
-        //         "hands" => $hands, ]
+        $game= ["location" => $table,
+                "hands" => $hands, ]
 
         ?>
     </body>
